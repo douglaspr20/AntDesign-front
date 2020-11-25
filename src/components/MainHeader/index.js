08e0344fe59d96dd5cd849cc5f4520b20a168595
love1119 <goldenstar007.dev@gmail.com>
2020-11-25 12:59:19 -0800

    create main header component

---
+++ src/components/MainHeader/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { SIDEBAR_MENU_LIST } from "enum";
+import CustomButtom from "../Button";
+
+import IconChevronDown from "images/icon-chevron-down.svg";
+
+import "./style.scss";
+
+const MenuList = [
+  ...SIDEBAR_MENU_LIST.TOP_MENUS,
+  ...SIDEBAR_MENU_LIST.BOTTOM_MENUS,
+];
+
+class MainHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {
+        img: null,
+        name: "Edgar davis",
+        abbrName: "ED",
+      },
+    };
+  }
+
+  render() {
+    const { user } = this.state;
+    const { pathname } = this.props.history.location || {};
+    const pathInfo = MenuList.find((item) => item.url === pathname);
+
+    return (
+      <div className="top-header">
+        <div className="top-header-left">
+          {pathInfo && (
+            <>
+              <div className="page-icon">
+                <img src={pathInfo.icon} alt="page-icon" />
+              </div>
+              <span className="page-label">{pathInfo.label}</span>
+            </>
+          )}
+        </div>
+        <div className="top-header-right">
+          <CustomButtom
+            text="Upgrade"
+            type="primary"
+            size="lg"
+            className="btn-upgrade"
+          />
+          <div className="user-avatar">
+            {user.img ? (
+              <img src={user.img} alt="user-avatar" />
+            ) : (
+              user.abbrName
+            )}
+          </div>
+          <span className="user-name">{user.name}</span>
+          <div className="profile-menu-chevron">
+            <img src={IconChevronDown} alt="profile-menu" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+MainHeader.propTypes = {
+  title: PropTypes.string,
+};
+
+MainHeader.defaultProps = {
+  title: "",
+};
+
+export default MainHeader;
