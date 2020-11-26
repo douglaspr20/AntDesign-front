ad9145bc62e851a2219338ab5af07cff1fa0f760
love1119 <goldenstar007.dev@gmail.com>
2020-11-25 18:19:17 -0800

    Define CircularProgressbar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Sidebar } from "./Sidebar";
 export { default as CustomButton } from "./Button";
 export { default as MainHeader } from "./MainHeader";
+export { default as CircularProgressbar } from "./CircularProgressbar";
