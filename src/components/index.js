b162ad2e363f9d555bcc64d132ce07ae62692c33
love1119 <goldenstar007.dev@gmail.com>
2020-11-26 02:07:48 -0800

    show events list on home page

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as Sidebar } from "./Sidebar";
 export { default as CustomButton } from "./Button";
 export { default as MainHeader } from "./MainHeader";
 export { default as CircularProgressbar } from "./CircularProgressbar";
+export { default as EventCard } from "./EventCard";
+export { default as DateAvatar } from "./DateAvatar";
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,110 @@
 import React from "react";
+import { Row, Col } from "antd";
+import groupBy from "lodash/groupBy";
+import moment from "moment";
+
+import ProfileStatusBar from "./ProfileStatusBar";
+import { DateAvatar, EventCard } from "components";
 
 import "./style.scss";
 
+const DataFormat = "YYYY.MM.DD hh:mm A";
+const EventsData = [
+  {
+    date: "2020.11.18 19:00 pm",
+    title: "Meetup - How to improve your soft skills",
+    timezone: "EST",
+    type: "Online event",
+    cost: "Free",
+    actionType: "attend",
+    img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+  },
+  {
+    date: "2020.11.18 19:00 pm",
+    title: "Meetup - Beers and HHRR after work",
+    timezone: "EST",
+    type: "Online event",
+    cost: "Free",
+    actionType: "going",
+    img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+  },
+  {
+    date: "2020.11.22 19:00 pm",
+    title: "Bay area job seekers and recruiters network skills",
+    timezone: "EST",
+    type: "Online event",
+    cost: "Free",
+    actionType: "attend",
+    img: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+  },
+];
+const monthStr = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
 class HomePage extends React.Component {
   render() {
+    const groupedByEventData = groupBy(EventsData, "date");
+    console.log("grup", groupedByEventData);
+
     return (
       <div className="home-page">
-        Home Page
+        <div className="home-page-profile">
+          <Row gutter={16}>
+            <Col lg={{ span: 16, offset: 4 }}>
+              <ProfileStatusBar percent={90} />
+            </Col>
+          </Row>
+        </div>
+        <div className="home-page-container">
+          <Row gutter={16}>
+            <Col lg={{ span: 16, offset: 4 }}>
+              <div className="d-flex justify-between">
+                <h3 className="text-bold">Events</h3>
+                <h3 className="color-primary view_all_events">
+                  View all events
+                </h3>
+              </div>
+            </Col>
+          </Row>
+          {Object.keys(groupedByEventData).map((date) => {
+            const day = moment(date, DataFormat).date();
+            const month = moment(date, DataFormat).month();
+            return (
+              <div className="event-card-group" key={date}>
+                <Row gutter={16}>
+                  <Col lg={{ span: 2, offset: 4 }}>
+                    <DateAvatar day={day} month={monthStr[month]} />
+                  </Col>
+                  <Col lg={{ span: 14 }}>
+                    <Row gutter={18}>
+                      {groupedByEventData[date].map((event, index) => (
+                        <Col
+                          key={`${date}-${index}`}
+                          span={24}
+                          className="event-card-item"
+                        >
+                          <EventCard data={event} />
+                        </Col>
+                      ))}
+                    </Row>
+                  </Col>
+                </Row>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
