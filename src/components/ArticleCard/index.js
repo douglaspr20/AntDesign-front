6e9079f6fd3339b6665616b6ce529553e539460b
love1119 <goldenstar007.dev@gmail.com>
2020-11-26 17:01:14 -0800

    Fix truncate issue on article card

--- src/components/ArticleCard/index.js
+++ src/components/ArticleCard/index.js
@@ -6,11 +6,37 @@ import IconDocument from "images/icon-document.svg";
 import "./style.scss";
 
 class ArticleCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lineClamp: 3,
+      randomId: `article-description-${Math.floor(Math.random() * 1000)}`,
+    };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.getRowNum();
+    }, 500);
+    window.addEventListener("resize", () => {
+      this.getRowNum();
+    });
+  }
+
+  getRowNum = () => {
+    const descElement = document.querySelector(`#${this.state.randomId}`);
+    if (descElement) {
+      const maxRow = Math.floor(descElement.offsetHeight / 23);
+      this.setState({ lineClamp: maxRow ? maxRow : 1 });
+    }
+  };
+
   render() {
     const {
       data: { img, title, desc },
       className,
     } = this.props;
+    const { randomId, lineClamp } = this.state;
     const newClassName = `article-card ${className || ""}`;
 
     return (
@@ -18,9 +44,18 @@ class ArticleCard extends React.Component {
         <div className="article-card-img">
           {img && <img src={img} alt="article-card-img" />}
         </div>
-        <div className="article-card-content d-flex flex-column justify-between items-start">
+        <div className="article-card-content">
           <h3>{title}</h3>
-          <h5>{desc}</h5>
+          <div id={randomId} className="d-flex items-center">
+            <p
+              style={{
+                WebkitLineClamp: lineClamp,
+                maxHeight: 23 * lineClamp,
+              }}
+            >
+              {desc}
+            </p>
+          </div>
           <div className="article-card-content-file">
             <div className="doc-image">
               <img src={IconDocument} alt="doc-icon" />
