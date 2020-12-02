ddd891121c7ff020c14dbce293e8363518f8cf12
love1119 <goldenstar007.dev@gmail.com>
2020-12-02 11:24:37 -0800

    customize radio button

--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 
 import "./style.scss";
 
-const CustomRadio = ({ type, className, size, children, ...params }) => {
-  const [checked, setChecked] = useState(false);
-
-  return (
-    <div
-      className={clsx("custom-radio", type, size, className, {
-        checked: checked,
-      })}
-      onClick={() => setChecked(!checked)}
-      {...params}
-    >
-      <div className="custom-radio-inner">
-        <i className="fas fa-check" />
-      </div>
-      <span className="custom-radio-text">{children}</span>
+const CustomRadio = ({
+  type,
+  className,
+  size,
+  checked,
+  children,
+  ...params
+}) => (
+  <div
+    className={clsx("custom-radio", type, size, className, {
+      checked: checked,
+    })}
+    {...params}
+  >
+    <div className="custom-radio-inner">
+      <i className="fas fa-check" />
     </div>
-  );
-};
+    <span className="custom-radio-text">{children}</span>
+  </div>
+);
 
 CustomRadio.propTypes = {
   type: PropTypes.string,
   size: PropTypes.string,
+  checked: PropTypes.bool,
   className: PropTypes.string,
-  onChange: PropTypes.func,
 };
 
 CustomRadio.defaultProps = {
   type: "primary",
   size: "md",
+  checked: false,
   className: "",
-  onChange: () => {},
 };
 
 export default CustomRadio;
