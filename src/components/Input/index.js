e3311e4dbe3a9e4d42270707cafb493ca61681f1
love1119 <goldenstar007.dev@gmail.com>
2020-12-01 19:38:13 -0800

    customize input

---
+++ src/components/Input/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { Input } from "antd";
+
+import "./style.scss";
+
+class CustomInput extends React.Component {
+  render() {
+    const { className, multiple, ...rest } = this.props;
+
+    return multiple ? (
+      <Input.TextArea
+        {...rest}
+        rows={4}
+        className={clsx("custom-input", className)}
+      />
+    ) : (
+      <Input {...rest} className={clsx("custom-input", className)} />
+    );
+  }
+}
+
+CustomInput.propTypes = {
+  className: PropTypes.string,
+  multiple: PropTypes.bool,
+};
+
+CustomInput.defaultProps = {
+  className: "",
+  multiple: false,
+};
+
+export default CustomInput;
