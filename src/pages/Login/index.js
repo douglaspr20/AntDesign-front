c14035f09ca25600925ec1723a9339f78acbdde1
love1119 <goldenstar007.dev@gmail.com>
2020-12-09 13:40:53 -0800

    update layout of login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,13 +2,25 @@ import React from "react";
 
 import { SignInButton } from "components";
 
+import IconLogo from "images/logo-sidebar.svg";
+
 import "./style.scss";
 
 class Login extends React.Component {
   render() {
     return (
       <div className="login-page">
-        <SignInButton text="Log In" type="primary" size="lg" />
+        <div className="login-dialog">
+          <div className="login-dialog-header">
+            <h3>Log In</h3>
+            <div className="login-dialog-logo">
+              <img src={IconLogo} alt="login-logo" />
+            </div>
+          </div>
+          <div className="login-dialog-footer">
+            <SignInButton text="Log In" type="primary" size="lg" />
+          </div>
+        </div>
       </div>
     );
   }
