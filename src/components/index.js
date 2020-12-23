fa73bf43ba6bf95738a8d6889c9781ed90d5952b
love1119 <goldenstar007.dev@gmail.com>
2020-12-23 14:00:00 -0800

    create card for first page in mentoring panel

--- src/components/index.js
+++ src/components/index.js
@@ -20,3 +20,4 @@ export { default as SignOutButton } from "./SignOutButton";
 export { default as PrivateRoute } from "./PrivateRoute";
 export { default as Tabs } from "./Tabs";
 export { default as NoEventCard } from "./NoEventCard";
+export { default as WantCard } from "./WantCard";
