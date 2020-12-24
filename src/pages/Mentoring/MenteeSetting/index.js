0afcc636c20190ee7c72ebc2c2ca2ae9ac6dbe7e
love1119 <goldenstar007.dev@gmail.com>
2020-12-24 14:31:08 -0800

    implement mobile responsive on mentoring page

--- src/pages/Mentoring/MenteeSetting/index.js
+++ src/pages/Mentoring/MenteeSetting/index.js
@@ -24,36 +24,38 @@ const MenteeSetting = ({ setting, onCancel, onSave }) => {
 
   return (
     <div className="mentee-setting">
-      <h2>Becoming into a mentee</h2>
-      <h5 className="mentee-setting-sublabel">
-        Why do you want to be a mentee?
-      </h5>
-      <CustomInput
-        className="mentee-setting-input"
-        multiple={true}
-        defaultValue={reason}
-        onChange={setReason}
-      />
-      <h5 className="mentee-setting-sublabel">
-        What is your current title / profession?
-      </h5>
-      <CustomInput
-        className="mentee-setting-input"
-        defaultValue={title}
-        onChange={setTitle}
-      />
-      <h5 className="mentee-setting-sublabel">What do you want to learn?</h5>
-      <Checkbox.Group
-        defaultValue={specialties}
-        className="mentee-setting-specialties"
-        onChange={setSpecialties}
-      >
-        {Specialties.map((spec, index) => (
-          <CustomCheckbox key={`specialty-${index}`} value={spec}>
-            {spec}
-          </CustomCheckbox>
-        ))}
-      </Checkbox.Group>
+      <div className="mentor-setting-container">
+        <h2>Becoming into a mentee</h2>
+        <h5 className="mentee-setting-sublabel first">
+          Why do you want to be a mentee?
+        </h5>
+        <CustomInput
+          className="mentee-setting-input"
+          multiple={true}
+          defaultValue={reason}
+          onChange={setReason}
+        />
+        <h5 className="mentee-setting-sublabel">
+          What is your current title / profession?
+        </h5>
+        <CustomInput
+          className="mentee-setting-input"
+          defaultValue={title}
+          onChange={setTitle}
+        />
+        <h5 className="mentee-setting-sublabel">What do you want to learn?</h5>
+        <Checkbox.Group
+          defaultValue={specialties}
+          className="mentee-setting-specialties"
+          onChange={setSpecialties}
+        >
+          {Specialties.map((spec, index) => (
+            <CustomCheckbox key={`specialty-${index}`} value={spec}>
+              {spec}
+            </CustomCheckbox>
+          ))}
+        </Checkbox.Group>
+      </div>
       <div className="mentee-setting-footer">
         <CustomButton
           text="Cancel"
