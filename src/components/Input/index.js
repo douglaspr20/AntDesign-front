5a56be502c632ffdc84b49c1690bc8b91dbb3066
love1119 <goldenstar007.dev@gmail.com>
2020-12-28 20:58:41 -0800

    built login page

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -7,19 +7,25 @@ import "./style.scss";
 
 class CustomInput extends React.Component {
   render() {
-    const { className, multiple, onChange, ...rest } = this.props;
+    const { className, multiple, type, onChange, size, ...rest } = this.props;
 
     return multiple ? (
       <Input.TextArea
         {...rest}
         rows={4}
-        className={clsx("custom-input", className, "mutiple")}
+        className={clsx("custom-input", className, "mutiple", size)}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    ) : type === "password" ? (
+      <Input.Password
+        {...rest}
+        className={clsx("custom-input", className, size)}
         onChange={(e) => onChange(e.target.value)}
       />
     ) : (
       <Input
         {...rest}
-        className={clsx("custom-input", className)}
+        className={clsx("custom-input", className, size)}
         onChange={(e) => onChange(e.target.value)}
       />
     );
@@ -29,12 +35,14 @@ class CustomInput extends React.Component {
 CustomInput.propTypes = {
   className: PropTypes.string,
   multiple: PropTypes.bool,
+  type: PropTypes.string,
   onChange: PropTypes.func,
 };
 
 CustomInput.defaultProps = {
   className: "",
   multiple: false,
+  type: "input",
   onChange: () => {},
 };
 
