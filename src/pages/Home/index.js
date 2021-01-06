5a8f228dcf2f1e065704771ad83a2a417dafea90
love1119 <goldenstar007.dev@gmail.com>
2021-01-05 18:45:19 -0800

    remove events section from homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { Row, Col } from "antd";
-import groupBy from "lodash/groupBy";
-import moment from "moment";
-import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 
-import { INTERNAL_LINKS } from "enum";
 import ProfileStatusBar from "./ProfileStatusBar";
-import { DateAvatar, EventCard, ArticleCard, CustomButton } from "components";
-import { updateEventData } from "redux/actions/home-actions";
+import { ArticleCard, CustomButton } from "components";
 import { homeSelector } from "redux/selectors/homeSelector";
 
 import "./style.scss";
-
-const DataFormat = "YYYY.MM.DD hh:mm A";
 
 const ArticlesData = [
   {
@@ -35,33 +28,7 @@ const ArticlesData = [
   },
 ];
 
-const monthStr = [
-  "JAN",
-  "FEB",
-  "MAR",
-  "APR",
-  "MAY",
-  "JUN",
-  "JUL",
-  "AUG",
-  "SEP",
-  "OCT",
-  "NOV",
-  "DEC",
-];
-
-const HomePage = ({ userProfile, events, updateEventData }) => {
-  const groupedByEventData = groupBy(events || [], "date");
-
-  const onEventChanged = (event, going) => {
-    let newEvents = events;
-    const index = newEvents.findIndex((item) => item.id === event.id);
-    if (index >= 0) {
-      newEvents[index] = { ...event, going };
-      updateEventData(events);
-    }
-  };
-
+const HomePage = ({ userProfile }) => {
   return (
     <div className="home-page">
       {userProfile && userProfile.percentOfCompletion !== 100 && (
@@ -76,53 +43,6 @@ const HomePage = ({ userProfile, events, updateEventData }) => {
         </div>
       )}
       <div className="home-page-container">
-        <Row gutter={16}>
-          <Col span={24} lg={{ span: 16, offset: 4 }}>
-            <div className="d-flex justify-between">
-              <h3 className="font-bold">Events</h3>
-              <NavLink to={INTERNAL_LINKS.EVENTS}>
-                <h3 className="color-primary view_all_events">
-                  View all events
-                </h3>
-              </NavLink>
-            </div>
-          </Col>
-        </Row>
-        {Object.keys(groupedByEventData).map((date) => {
-          const day = moment(date, DataFormat).date();
-          const month = moment(date, DataFormat).month();
-          return (
-            <div className="event-card-group" key={date}>
-              <Row gutter={[16, 20]}>
-                <Col span={24} md={{ span: 2 }} lg={{ span: 2, offset: 4 }}>
-                  <div className="d-flex justify-center items-center">
-                    <DateAvatar day={day} month={monthStr[month]} />
-                  </div>
-                </Col>
-                <Col
-                  span={24}
-                  md={{ span: 21, offset: 1 }}
-                  lg={{ span: 14, offset: 0 }}
-                >
-                  <Row gutter={18}>
-                    {groupedByEventData[date].map((event, index) => (
-                      <Col
-                        key={`${date}-${index}`}
-                        span={24}
-                        className="event-card-item"
-                      >
-                        <EventCard
-                          data={event}
-                          onAttend={(going) => onEventChanged(event, going)}
-                        />
-                      </Col>
-                    ))}
-                  </Row>
-                </Col>
-              </Row>
-            </div>
-          );
-        })}
         <Row gutter={16}>
           <Col lg={{ span: 16, offset: 4 }}>
             <h3 className="list-label">Recommended of the month</h3>
@@ -168,11 +88,8 @@ const HomePage = ({ userProfile, events, updateEventData }) => {
 
 const mapStateToProps = (state, props) => ({
   userProfile: homeSelector(state).userProfile,
-  events: homeSelector(state).events,
 });
 
-const mapDispatchToProps = {
-  updateEventData,
-};
+const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
