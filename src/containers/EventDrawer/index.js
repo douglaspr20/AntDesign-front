56a4f3bc6ad622d12ca53c837138119320e32998
love1119 <goldenstar007.dev@gmail.com>
2021-01-29 15:03:04 -0800

    hide calendar invite

--- src/containers/EventDrawer/index.js
+++ src/containers/EventDrawer/index.js
@@ -46,13 +46,13 @@ const EventDrawer = ({
 
   const onClickClaimCredits = (e) => {};
 
-  const menu = (
-    <Menu>
-      <Menu.Item>Google</Menu.Item>
-      <Menu.Item>Google</Menu.Item>
-      <Menu.Item>Google</Menu.Item>
-    </Menu>
-  );
+  // const menu = (
+  //   <Menu>
+  //     <Menu.Item>Google</Menu.Item>
+  //     <Menu.Item>Google</Menu.Item>
+  //     <Menu.Item>Google</Menu.Item>
+  //   </Menu>
+  // );
 
   const planUpgrade = () => {
     Emitter.emit(EVENT_TYPES.OPEN_PAYMENT_MODAL);
@@ -144,13 +144,13 @@ const EventDrawer = ({
             <div className="d-flex items-center">
               <h3 className="event-date">{event.period}</h3>
             </div>
-            {event.status === "going" && event.status !== "past" && (
+            {/* {event.status === "going" && event.status !== "past" && (
               <Dropdown overlay={menu}>
                 <h3 className="add-to-calendar ant-dropdown-link">
                   Add to calendar
                 </h3>
               </Dropdown>
-            )}
+            )} */}
           </div>
           <h3 className="event-type">{`${event.location} event`}</h3>
           <h3 className="event-cost">{event.ticket}</h3>
