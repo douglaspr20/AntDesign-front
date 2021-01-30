976f29eba7b2f084f364e1c7ed30dcac4ae33f3f
love1119 <goldenstar007.dev@gmail.com>
2021-01-29 18:58:06 -0800

    fix text box issue on public event page

--- src/containers/EventDrawer/index.js
+++ src/containers/EventDrawer/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { CheckOutlined } from "@ant-design/icons";
-import { Menu, Dropdown } from "antd";
 
 import {
   DateAvatar,
