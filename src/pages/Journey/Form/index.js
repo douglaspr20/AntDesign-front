f159c28c8b6da453747edaa29f81b947157fa9dc
Juan Camilo Martinez Morales <xogost@gmail.com>
2021-02-23 21:28:18 -0500

    The description field was added

--- src/pages/Journey/Form/index.js
+++ src/pages/Journey/Form/index.js
@@ -21,7 +21,7 @@ const JourneyForm = ({
   journey,
 }) => {
   return (<div className="learning-journey-form">
-    <h3>{ journey != null ? 'Update' : 'Create' } learning journey</h3>
+    <h3>{journey != null ? 'Update' : 'Create'} learning journey</h3>
 
     <Form
       layout="vertical"
@@ -35,70 +35,81 @@ const JourneyForm = ({
         <CustomInput></CustomInput>
       </Form.Item>
       <Form.Item
-        label="What do you want to focus on?"
-        name="topics"
+        label="Description"
+        name="description"
       >
-        <Checkbox.Group>
-          <Row>
-            {allCategories.map((item) => (
-              <Col key={`column-${item.value}`} span={8}>
-                <CustomCheckbox
-                  key={item.value}
-                  value={item.value}
-                  size="sm"
-                >
-                  {item.title}
-                </CustomCheckbox>
-              </Col>
-            ))}
-          </Row>
-        </Checkbox.Group>
+        <CustomInput multiple={true} ></CustomInput>
       </Form.Item>
-      <Form.Item
-        label="What content type you like to learn more?"
-        name="contentType"
-      >
-        <Checkbox.Group>
-          <Row>
-            <Col span={8}>
-              <CustomCheckbox
-                key={'article'}
-                value={'article'}
-                size="sm"
-              >
-                Article
+      {
+        journey == null &&
+        <>
+          <Form.Item
+            label="What do you want to focus on?"
+            name="topics"
+          >
+            <Checkbox.Group>
+              <Row>
+                {allCategories.map((item) => (
+                  <Col key={`column-${item.value}`} span={8}>
+                    <CustomCheckbox
+                      key={item.value}
+                      value={item.value}
+                      size="sm"
+                    >
+                      {item.title}
+                    </CustomCheckbox>
+                  </Col>
+                ))}
+              </Row>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item
+            label="What content type you like to learn more?"
+            name="contentType"
+          >
+            <Checkbox.Group>
+              <Row>
+                <Col span={8}>
+                  <CustomCheckbox
+                    key={'article'}
+                    value={'article'}
+                    size="sm"
+                  >
+                    Article
               </CustomCheckbox>
-            </Col>
-            <Col span={8}>
-              <CustomCheckbox
-                key={'video'}
-                value={'video'}
-                size="sm"
-              >
-                Video
+                </Col>
+                <Col span={8}>
+                  <CustomCheckbox
+                    key={'video'}
+                    value={'video'}
+                    size="sm"
+                  >
+                    Video
               </CustomCheckbox>
-            </Col>
-            <Col span={8}>
-              <CustomCheckbox
-                key={'event'}
-                value={'event'}
-                size="sm"
-              >
-                Event
+                </Col>
+                <Col span={8}>
+                  <CustomCheckbox
+                    key={'event'}
+                    value={'event'}
+                    size="sm"
+                  >
+                    Event
               </CustomCheckbox>
-            </Col>
-            <Col span={8}>
-              <CustomCheckbox
-                key={'podcast'}
-                value={'podcast'}
-                size="sm"
-              >
-                Podcast
+                </Col>
+                <Col span={8}>
+                  <CustomCheckbox
+                    key={'podcast'}
+                    value={'podcast'}
+                    size="sm"
+                  >
+                    Podcast
               </CustomCheckbox>
-            </Col>
-          </Row>
-        </Checkbox.Group>
-      </Form.Item>
+                </Col>
+              </Row>
+            </Checkbox.Group>
+          </Form.Item>
+        </>
+      }
       <Form.Item>
         <div className="mentor-setting-footer">
           <CustomButton
@@ -129,8 +140,8 @@ JourneyForm.propTypes = {
 
 JourneyForm.defaultProps = {
   allCategories: [],
-  onSave: () => {},
-  onCancel: () => {},
+  onSave: () => { },
+  onCancel: () => { },
   journey: null,
 };
 
