90b6a83248f0f4b928d76ac03ce29874b99108e4
love1119 <goldenstar007.dev@gmail.com>
2021-02-24 19:32:09 -0800

    remove annual conference menu from left-hand bar

--- src/enum/SidebarMenuList.js
+++ src/enum/SidebarMenuList.js
@@ -5,7 +5,7 @@ import IconLibrary from "images/icon-library.svg";
 import IconMentoring from "images/icon-mentoring.svg";
 import IconCalendar from "images/icon-calendar.svg";
 import IconMedal from "images/icon-medal.svg";
-import IconReader from "images/icon-reader.svg";
+// import IconReader from "images/icon-reader.svg";
 import IconStar from "images/icon-star.svg";
 import IconJourney from "images/icon-learning-journey.svg";
 import IconBookmark from "images/icon-bookmark.svg";
@@ -52,15 +52,15 @@ export default {
       url: INTERNAL_LINKS.CERTIFICATIONS,
     },
     {
-      label: "Journey",
+      label: "Learning Journeys",
       icon: IconJourney,
       url: INTERNAL_LINKS.JOURNEY,
     },
-    {
-      label: "Annual Conference",
-      icon: IconReader,
-      url: INTERNAL_LINKS.HUB,
-    },
+    // {
+    //   label: "Annual Conference",
+    //   icon: IconReader,
+    //   url: INTERNAL_LINKS.HUB,
+    // },
     {
       label: "Podcast",
       icon: IconHeadsetOutline,
