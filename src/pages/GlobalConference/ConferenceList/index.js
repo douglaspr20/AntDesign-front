17dbfa378827bd351ff7e288f655d25c9208cfe4
love1119 <goldenstar007.dev@gmail.com>
2021-05-02 23:01:17 -0700

    display list of sessions

---
+++ src/pages/GlobalConference/ConferenceList/index.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import moment from "moment";
+
+import { CustomSelect, AnnualConferenceCard } from "components";
+import { CONFERENCE_SETTING } from "enum";
+
+import { categorySelector } from "redux/selectors/categorySelector";
+
+import "./style.scss";
+
+const SessionType = [
+  {
+    text: "All Sessions",
+    value: "all",
+  },
+  ...CONFERENCE_SETTING.SESSION_TYPE,
+];
+
+const ConferenceData = [
+  {
+    title: "Leading organizations in the remote world of work",
+    type: "Keybote",
+    startTime: moment(),
+    endTime: moment(),
+    timezone: "Pacific Daylight Time",
+  },
+  {
+    title: "Leading organizations in the remote world of work",
+    type: "Keybote",
+    startTime: moment(),
+    endTime: moment(),
+    timezone: "Pacific Daylight Time",
+  },
+  {
+    title: "Leading organizations in the remote world of work",
+    type: "Keybote",
+    startTime: moment(),
+    endTime: moment(),
+    timezone: "Pacific Daylight Time",
+  },
+  {
+    title: "Leading organizations in the remote world of work",
+    type: "Keybote",
+    startTime: moment(),
+    endTime: moment(),
+    timezone: "Pacific Daylight Time",
+  },
+];
+
+const ConferenceList = ({ data, allCategories }) => {
+  const [sortTheme, setSortTheme] = useState("main");
+  const [sortCategory, setSortCategory] = useState("all");
+  const [sortSessionType, setSortSessionType] = useState("all");
+  const [categoryOptions, setCategoryOptions] = useState([
+    {
+      text: "All Categories",
+      value: "all",
+    },
+  ]);
+
+  useEffect(() => {
+    setCategoryOptions([
+      {
+        text: "All Categories",
+        value: "all",
+      },
+      ...allCategories.map((category) => ({
+        text: category.title,
+        value: category.value,
+      })),
+    ]);
+  }, [allCategories]);
+
+  return (
+    <div className="conference-list">
+      <div className="conference-list-header">
+        <h3>{`${data.length} talk${data.length === 1 ? "" : "s"}`}</h3>
+        <div className="conference-list-header-select">
+          <CustomSelect
+            className="conference-list-header-sort"
+            bordered={false}
+            options={[{ value: "main", text: "Main theme" }]}
+            value={sortTheme}
+            onChange={setSortTheme}
+          />
+          <CustomSelect
+            className="conference-list-header-sort"
+            bordered={false}
+            options={categoryOptions}
+            value={sortCategory}
+            onChange={setSortCategory}
+          />
+          <CustomSelect
+            className="conference-list-header-sort"
+            bordered={false}
+            options={SessionType}
+            value={sortSessionType}
+            onChange={setSortSessionType}
+          />
+        </div>
+      </div>
+      <div className="conference-list-container">
+        {ConferenceData.map((session, index) => (
+          <AnnualConferenceCard key={index} session={session} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+ConferenceList.propTypes = {
+  data: PropTypes.array,
+};
+
+ConferenceList.defaultProps = {
+  data: [],
+};
+
+const mapStateToProps = (state) => ({
+  allCategories: categorySelector(state).categories,
+});
+
+export default connect(mapStateToProps)(ConferenceList);
--- src/pages/GlobalConference/index.js
+++ src/pages/GlobalConference/index.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+
+import { CustomButton, Tabs } from "components";
+
+import ConferenceList from "./ConferenceList";
 
 import "./style.scss";
 
+const Description = `
+  Developing Talent & Leadership behaviors. Positive Design Thinking & Strategy through Positive Leadership Strategy and POSITIVE & AGILE coaching | 2 hack habits, goal achievement, and behavior transformation in organizations, sports clubs, PYMES, and corporations.
+`;
+
 const GlobalConference = ({ title }) => {
+  const [currentTab, setCurrentTab] = useState("0");
+
+  const TabData = [
+    {
+      title: "March 07",
+      content: () => <ConferenceList />,
+    },
+    {
+      title: "March 08",
+      content: () => <ConferenceList />,
+    },
+    {
+      title: "March 09",
+      content: () => <ConferenceList />,
+    },
+    {
+      title: "March 10",
+      content: () => <ConferenceList />,
+    },
+    {
+      title: "March 11",
+      content: () => <ConferenceList />,
+    },
+  ];
+
   return (
     <div className="global-conference">
-      Coming soon
+      <div className="global-conference-container">
+        <div className="w-full d-flex justify-between items-center">
+          <h3>HHRR Global Conference 2021</h3>
+          <CustomButton size="xs" text="Attend the conference" />
+        </div>
+        <p className="global-conference-description">{Description}</p>
+        <div className="global-conference-tabs">
+          <Tabs data={TabData} current={currentTab} onChange={setCurrentTab} />
+        </div>
+      </div>
     </div>
   );
 };
