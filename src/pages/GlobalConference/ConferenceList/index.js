fb60f22334f96b21befc119fa19741ab69790bf6
love1119 <goldenstar007.dev@gmail.com>
2021-05-20 18:44:38 -0700

    group session list by step

--- src/pages/GlobalConference/ConferenceList/index.js
+++ src/pages/GlobalConference/ConferenceList/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import isEmpty from "lodash/isEmpty";
 
 import { CustomSelect, AnnualConferenceCard } from "components";
 import { CONFERENCE_SETTING, TIMEZONE_LIST } from "enum";
@@ -8,7 +9,7 @@ import { CONFERENCE_SETTING, TIMEZONE_LIST } from "enum";
 import { categorySelector } from "redux/selectors/categorySelector";
 import { homeSelector } from "redux/selectors/homeSelector";
 import { addSession, removeSession } from "redux/actions/home-actions";
-import { convertToCertainTime } from "utils/format";
+import { convertToCertainTime, convertToLocalTime } from "utils/format";
 
 import "./style.scss";
 
@@ -18,6 +19,39 @@ const SessionType = [
     value: "all",
   },
   ...CONFERENCE_SETTING.SESSION_TYPE,
+];
+
+const TimeSegments = [
+  {
+    from: 0,
+    to: 9,
+    format: "0.00 am to 9.00 am ",
+  },
+  {
+    from: 9,
+    to: 12,
+    format: "9.00 am to 12.00 am ",
+  },
+  {
+    from: 12,
+    to: 13,
+    format: "12.00 pm to 1.00 pm ",
+  },
+  {
+    from: 13,
+    to: 15,
+    format: "1.00 pm to 3.00 pm ",
+  },
+  {
+    from: 15,
+    to: 17,
+    format: "3.00 pm to 5.00 pm ",
+  },
+  {
+    from: 17,
+    to: 24,
+    format: "5.00 pm to 0.00 am ",
+  },
 ];
 
 const ConferenceList = ({
@@ -48,32 +82,44 @@ const ConferenceList = ({
 
   useEffect(() => {
     if (data) {
-      setSessionData(
-        (data || []).map((item) => {
-          const sTime = convertToCertainTime(item.startTime, item.timezone);
-          const eTime = convertToCertainTime(item.endTime, item.timezone);
-          let tz = TIMEZONE_LIST.find((t) => t.value === item.timezone);
-          if (tz) {
-            if (tz.offset > 0) {
-              tz = `${tz.abbr} (GMT+${tz.offset})`;
-            } else if (tz.offset < 0) {
-              tz = `${tz.abbr} (GMT-${-tz.offset})`;
-            } else {
-              tz = `${tz.abbr} (GMT)`;
-            }
+      const sData = (data || []).map((item) => {
+        const sTime = convertToCertainTime(item.startTime, item.timezone);
+        const eTime = convertToCertainTime(item.endTime, item.timezone);
+        let tz = TIMEZONE_LIST.find((t) => t.value === item.timezone);
+        if (tz) {
+          if (tz.offset > 0) {
+            tz = `${tz.abbr} (GMT+${tz.offset})`;
+          } else if (tz.offset < 0) {
+            tz = `${tz.abbr} (GMT-${-tz.offset})`;
           } else {
-            tz = "";
+            tz = `${tz.abbr} (GMT)`;
           }
+        } else {
+          tz = "";
+        }
 
-          return {
-            ...item,
-            date: sTime.format("MMM, D, YYYY"),
-            period: `From ${sTime.format("h:mm a")} to ${eTime.format(
-              "h:mm a"
-            )} ${tz}`,
-          };
-        })
-      );
+        return {
+          ...item,
+          date: sTime.format("MMM, D, YYYY"),
+          period: `From ${sTime.format("h:mm a")} to ${eTime.format(
+            "h:mm a"
+          )} ${tz}`,
+        };
+      });
+      const filteredData = TimeSegments.map((step) => {
+        const gSessions = sData.filter((item) => {
+          const localTime = convertToLocalTime(item.startTime).hours();
+
+          return localTime >= step.from && localTime < step.to;
+        });
+
+        return {
+          step: step.format,
+          data: gSessions,
+        };
+      });
+
+      setSessionData(filteredData);
     } else {
       setSessionData([]);
     }
@@ -120,16 +166,23 @@ const ConferenceList = ({
         </div>
       </div>
       <div className="conference-list-container">
-        {sessionData.map((session, index) => (
-          <AnnualConferenceCard
-            key={index}
-            session={session}
-            attended={userProfile.attendedToConference}
-            added={(userProfile.sessions || []).includes(session.id)}
-            onAddSession={() => onAddSession(session)}
-            onRemoveSession={() => onRemoveSession(session)}
-          />
-        ))}
+        {sessionData
+          .filter((item) => !isEmpty(item.data))
+          .map((session, index) => (
+            <div key={index}>
+              <h3 className="session-step">{session.step}</h3>
+              {session.data.map((s) => (
+                <AnnualConferenceCard
+                  key={s.id}
+                  session={s}
+                  attended={userProfile.attendedToConference}
+                  added={(userProfile.sessions || []).includes(s.id)}
+                  onAddSession={() => onAddSession(s)}
+                  onRemoveSession={() => onRemoveSession(s)}
+                />
+              ))}
+            </div>
+          ))}
       </div>
     </div>
   );
