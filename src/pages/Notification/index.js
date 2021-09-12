b20549552d7de33382db1e28f356e3449cfe17b0
love1119 <goldenstar007.dev@gmail.com>
2021-09-12 10:31:12 -0700

    add checkbox to notifications list

--- src/pages/Notification/index.js
+++ src/pages/Notification/index.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import { Table } from "antd";
 
 import { CustomButton } from "components";
 import { INTERNAL_LINKS } from "enum";
@@ -30,6 +31,14 @@ const NotificationPage = ({
   getNotifications,
   markNotificationToRead,
 }) => {
+  const Columns = [
+    {
+      title: "",
+      dataIndex: "description",
+      render: (action, data) => renderNotification(data),
+    },
+  ];
+
   useEffect(() => {
     getNotifications(1, MAX_NOTIFICATIONS);
 
@@ -42,71 +51,85 @@ const NotificationPage = ({
 
   const renderLoading = () => <div className="loading-container" />;
 
+  const renderNotification = (noti) => {
+    return (
+      <div className="notification-list-item" key={noti.id}>
+        <div className="notification-list-item-left">
+          <div className="notification-list-item-message">
+            {!noti.readers.includes(userProfile.id) && (
+              <div className="notification-list-item-circle" />
+            )}
+            <h4>{noti.message}</h4>
+          </div>
+          <h6 className="notification-list-item-date">
+            {moment(noti.createdAt).format("YYYY, MMM DD h:mm a")}
+          </h6>
+        </div>
+        {noti.type === "event" && (
+          <a
+            className="notification-list-item-link"
+            href={noti.meta.publicLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => markNotificationToRead([noti.id], userProfile.id)}
+          >
+            Go to event
+          </a>
+        )}
+        {noti.type === "podcast" && (
+          <Link
+            className="notification-list-item-link"
+            to={INTERNAL_LINKS.PODCAST}
+            onClick={() => markNotificationToRead([noti.id], userProfile.id)}
+          >
+            Go to Podcast
+          </Link>
+        )}
+        {noti.type === "marketplace" && (
+          <Link
+            className="notification-list-item-link"
+            to={INTERNAL_LINKS.MARKETPLACE}
+            onClick={() => markNotificationToRead([noti.id], userProfile.id)}
+          >
+            Go to HR Marketplace
+          </Link>
+        )}
+        {noti.type === "content" && (
+          <Link
+            className="notification-list-item-link"
+            to={INTERNAL_LINKS.LEARNING_LIBRARY}
+            onClick={() => markNotificationToRead([noti.id], userProfile.id)}
+          >
+            Go to Learning library
+          </Link>
+        )}
+      </div>
+    );
+  };
+
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      console.log("**** selectedRowKeys ", selectedRowKeys);
+      console.log("**** selectedRows ", selectedRows);
+    },
+  };
+
   const renderNotifications = () => (
     <>
       {notificationList.length > 0 ? (
         <>
-          {notificationList.map((noti) => (
-            <div className="notification-list-item" key={noti.id}>
-              <div className="notification-list-item-left">
-                <div className="notification-list-item-message">
-                  {!noti.readers.includes(userProfile.id) && (
-                    <div className="notification-list-item-circle" />
-                  )}
-                  <h4>{noti.message}</h4>
-                </div>
-                <h6 className="notification-list-item-date">
-                  {moment(noti.createdAt).format("YYYY, MMM DD h:mm a")}
-                </h6>
-              </div>
-              {noti.type === "event" && (
-                <a
-                  className="notification-list-item-link"
-                  href={noti.meta.publicLink}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={() =>
-                    markNotificationToRead([noti.id], userProfile.id)
-                  }
-                >
-                  Go to event
-                </a>
-              )}
-              {noti.type === "podcast" && (
-                <Link
-                  className="notification-list-item-link"
-                  to={INTERNAL_LINKS.PODCAST}
-                  onClick={() =>
-                    markNotificationToRead([noti.id], userProfile.id)
-                  }
-                >
-                  Go to Podcast
-                </Link>
-              )}
-              {noti.type === "marketplace" && (
-                <Link
-                  className="notification-list-item-link"
-                  to={INTERNAL_LINKS.MARKETPLACE}
-                  onClick={() =>
-                    markNotificationToRead([noti.id], userProfile.id)
-                  }
-                >
-                  Go to HR Marketplace
-                </Link>
-              )}
-              {noti.type === "content" && (
-                <Link
-                  className="notification-list-item-link"
-                  to={INTERNAL_LINKS.LEARNING_LIBRARY}
-                  onClick={() =>
-                    markNotificationToRead([noti.id], userProfile.id)
-                  }
-                >
-                  Go to Learning library
-                </Link>
-              )}
-            </div>
-          ))}
+          <Table
+            rowSelection={{
+              type: "checkbox",
+              ...rowSelection,
+            }}
+            className="notification-table"
+            columns={Columns}
+            dataSource={notificationList}
+            pagination={false}
+            bordered={false}
+            rowKey="id"
+          />
           {currentPage * MAX_NOTIFICATIONS < countOfResults && (
             <div className="notification-page-showmore d-flex justify-center items-center">
               {moreLoading && (
