c25426d36ab4e27aaf7615addf055febc71aeb9f
lourencelinao13 <lourencelinao13@gmail.com>
2021-11-10 11:51:26 +0800

    [FEATURE][LOU] - SKILL COHORT

--- src/redux/reducers/skillCohortParticipantReducer.js
+++ src/redux/reducers/skillCohortParticipantReducer.js
@@ -1,22 +1,28 @@
-import { handleActions } from 'redux-actions';
-import { Map } from 'immutable';
+import { handleActions } from "redux-actions";
+import { Map } from "immutable";
 
-import { constants as skillCohortParticipanActions } from 'redux/actions/skillCohortParticipant-actions';
+import { constants as skillCohortParticipanActions } from "redux/actions/skillCohortParticipant-actions";
 
 const reducers = {
-	[skillCohortParticipanActions.SET_SKILL_COHORT_PARTICIPANT]: (state, { payload }) => {
-		return state.merge({ ...payload });
-	},
-	[skillCohortParticipanActions.SET_ALL_SKILL_COHORT_PARTICIPANTS]: (state, { payload }) => {
-		return state.merge({ ...payload });
-	},
+  [skillCohortParticipanActions.SET_SKILL_COHORT_PARTICIPANT]: (
+    state,
+    { payload }
+  ) => {
+    return state.merge({ ...payload });
+  },
+  [skillCohortParticipanActions.SET_ALL_SKILL_COHORT_PARTICIPANTS]: (
+    state,
+    { payload }
+  ) => {
+    return state.merge({ ...payload });
+  },
 };
 
 export const initialState = () => {
-	return Map({
-		allSkillCohortParticipants: [],
-		skillCohortParticipant: {},
-	});
+  return Map({
+    allSkillCohortParticipants: [],
+    skillCohortParticipant: {},
+  });
 };
 
 export default handleActions(reducers, initialState());
