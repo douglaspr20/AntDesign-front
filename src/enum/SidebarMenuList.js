1c98df1f835115769d793513d7ea641e3d46857c
Juan Camilo Martinez Morales <xogost@gmail.com>
2021-11-10 19:59:41 -0500

    The project X menu was disabled

--- src/enum/SidebarMenuList.js
+++ src/enum/SidebarMenuList.js
@@ -15,7 +15,7 @@ import IconStoreFrontOutline from "images/icon-storefront-outline.svg";
 import IconConferenceVideo from "images/icon-sidebar-video.svg";
 import IconTvOutline from "images/icon-tv-outline.svg";
 import IconGlobal from "images/icon-global.svg";
-import IconFlask from "images/icon-flask-outline.svg"
+// import IconFlask from "images/icon-flask-outline.svg"
 // import IconGlobal from "images/icon-global.svg";
 
 export default {
@@ -35,11 +35,11 @@ export default {
       icon: IconTvOutline,
       url: INTERNAL_LINKS.CHANNELS,
     },
-    {
+    /* {
       label: "ProjectX",
       icon: IconFlask,
       url: INTERNAL_LINKS.SKILL_COHORTS,
-    },
+    }, */
     {
       label: "Learning library",
       icon: IconLibrary,
