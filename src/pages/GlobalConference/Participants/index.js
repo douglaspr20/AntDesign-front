229af3bbde8eaf66399659bdf0af9e242de70a7b
Douglas Pérez <douglas.eduardo2000@gmail.com>
2021-11-14 11:24:52 -0400

    first commit

--- src/pages/GlobalConference/Participants/index.js
+++ src/pages/GlobalConference/Participants/index.js
@@ -1,20 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { sessionSelector } from "redux/selectors/sessionSelector";
+import { bonfireSelector } from "redux/selectors/bonfireSelector";
+import { getBonfires } from "redux/actions/bonfire-actions";
 import { homeSelector } from "redux/selectors/homeSelector";
 import { getParticipants } from "redux/actions/session-actions";
-import { ParticipantCard } from "components";
-
+import { CustomButton, ParticipantCard } from "components";
+import { Modal, List, Skeleton } from "antd";
 import "./style.scss";
 
-const Participants = ({ participants, userProfile, getParticipants }) => {
+const Participants = ({
+  participants,
+  bonfires,
+  userProfile,
+  getParticipants,
+  getBonfires,
+}) => {
+  const [openModal, setOpenModal] = useState(false);
+  const [participantToInvite, setParticipantToInvite] = useState(null);
   useEffect(() => {
     if (userProfile.topicsOfInterest.length > 0) {
       getParticipants({
         topics: userProfile.topicsOfInterest,
       });
+
+      getBonfires();
     }
-  }, [getParticipants, userProfile]);
+  }, [getParticipants, getBonfires, userProfile]);
+
+  const onOpenModalBonfires = (participantId) => {
+    setOpenModal(true);
+    setParticipantToInvite(participantId);
+  };
+
+  console.log(bonfires);
 
   return (
     <div className="participants">
@@ -22,10 +41,65 @@ const Participants = ({ participants, userProfile, getParticipants }) => {
         <h2>{participants.length} Recommended Participants To Connect With</h2>
         <div className="participants-list">
           {participants.map((participant, i) => (
-            <ParticipantCard key={i} participant={participant} />
+            <ParticipantCard
+              key={i}
+              participant={participant}
+              onOpenModalBonfires={onOpenModalBonfires}
+            />
           ))}
         </div>
       </div>
+      <Modal
+        title="Bonfires creators for you"
+        centered
+        visible={openModal}
+        onOk={() => setOpenModal(false)}
+        onCancel={() => setOpenModal(false)}
+        width={1000}
+      >
+        <List
+          itemLayout="horizontal"
+          dataSource={bonfires.filter(
+            (bonfire) => bonfire.bonfireCreator === userProfile.id
+          )}
+          renderItem={(bonfire) => {
+            if (
+              bonfire.invitedUsers.includes(participantToInvite) ||
+              bonfire.uninvitedJoinedUsers.includes(participantToInvite)
+            ) {
+              return (
+                <List.Item
+                  actions={[
+                    <CustomButton
+                      size="sm"
+                      text="User Already Invited or Joined"
+                      disabled={true}
+                    />,
+                  ]}
+                >
+                  <Skeleton title={false} loading={false}>
+                    <List.Item.Meta
+                      title={bonfire.title}
+                      description={bonfire.description}
+                    />
+                  </Skeleton>
+                </List.Item>
+              );
+            }
+
+            return (
+              <List.Item extra={<CustomButton size="sm" text="Invite User" />}>
+                <Skeleton title={false} loading={false}>
+                  <List.Item.Meta
+                    title={bonfire.title}
+                    description={bonfire.description}
+                  />
+                </Skeleton>
+              </List.Item>
+            );
+          }}
+        />
+      </Modal>
     </div>
   );
 };
@@ -33,10 +107,12 @@ const Participants = ({ participants, userProfile, getParticipants }) => {
 const mapStateToProps = (state) => ({
   ...sessionSelector(state),
   userProfile: homeSelector(state).userProfile,
+  bonfires: bonfireSelector(state).bonfires,
 });
 
 const mapDispatchToProps = {
   getParticipants,
+  getBonfires,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Participants);
