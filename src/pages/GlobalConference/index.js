b7b7c277c10401ac878bb49614cc1b43a0d42928
Douglas Pérez <douglas.eduardo2000@gmail.com>
2022-01-05 12:57:50 -0400

    change date

--- src/pages/GlobalConference/index.js
+++ src/pages/GlobalConference/index.js
@@ -94,7 +94,7 @@ const GlobalConference = ({
   const [colleaguesForm] = Form.useForm();
 
   const [currentTab, setCurrentTab] = useState("0");
-  const [firstTabDate] = useState(moment("2022-01-04", "YYYY-MM-DD"));
+  const [firstTabDate] = useState(moment("2022-03-07", "YYYY-MM-DD"));
   const [tabData, setTabData] = useState([]);
   const [filters, setFilters] = useState({});
   const [meta, setMeta] = useState("");
