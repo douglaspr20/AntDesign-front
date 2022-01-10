af22084214b77148288138ffb63a7b02b038bfc6
Betsy Moreno <morenoelba2002@gmail.com>
2022-01-10 10:47:51 -0400

    fix: councils participants icons cards

--- src/pages/Council/CouncilParticipantsCards/index.js
+++ src/pages/Council/CouncilParticipantsCards/index.js
@@ -3,7 +3,6 @@ import { Card, Avatar, Tooltip, Button } from "antd";
 import {
   LinkedinOutlined,
   MailOutlined,
-  ProfileOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import { connect } from "react-redux";
@@ -17,75 +16,50 @@ const CouncilParticipantCard = ({
   userProfile,
   marketplaceProfile,
 }) => {
-  const downloadFile = () => {
-    const link = document.createElement("a");
-    link.setAttribute("href", participant.resumeUrl);
-    link.setAttribute(
-      "download",
-      `${participant.firstName} ${participant.lastName}`
-    );
-    link.setAttribute("target", "_blank");
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
     <>
-      {participant.img ? (
-        <Avatar size={150} src={participant.img} />
-      ) : (
-        <Avatar size={150} style={{ fontSize: "2rem" }}>
-          {participant.abbrName}
-        </Avatar>
-      )}
-
-      <div style={{ textAlign: "center" }}>
-        <p style={{ fontWeight: 500 }}>
-          {participant.firstName} {participant.lastName}
-        </p>
-        {participant.lookingFor ? (
-          <div style={{ marginTop: -10 }}>
-            <span className="participant-card-marketplaceprofile--title">
-              Looking For:
-            </span>
-            {participant.lookingFor.map((profesion, i) => (
-              <SpecialtyItem key={i} title={profesion} />
-            ))}
-          </div>
+      <div className="council-participant-container">
+        {participant.img ? (
+          <Avatar size={150} src={participant.img} />
         ) : (
-          <p style={{ marginTop: -10 }}>{participant.titleProfessions}</p>
+          <Avatar size={150} style={{ fontSize: "2rem" }}>
+            {participant.abbrName}
+          </Avatar>
         )}
-        <p style={{ marginTop: -10 }}>{participant.company}</p>
 
-        {participant.location && marketplaceProfile && (
-          <div style={{ marginBottom: "50px" }}>
-            <span className="participant-card-marketplaceprofile--title">
-              Available locations:
-            </span>
-
-            {participant.location.map((location, i) => (
-              <SpecialtyItem key={i} title={location} />
-            ))}
-          </div>
-        )}
-      </div>
-      {marketplaceProfile && (
-        <div className="participant-card-marketplaceprofile">
-          {participant.resumeUrl !== "" && participant.resumeUrl && (
-            <Tooltip title="Download Resume">
-              <Button
-                shape="circle"
-                type="link"
-                icon={<ProfileOutlined />}
-                onClick={() => downloadFile()}
-                disabled={userProfile.memberShip !== "premium"}
-                className="participant-card-marketplaceprofile-icon"
-              />
-            </Tooltip>
+        <div style={{ textAlign: "center" }}>
+          <p style={{ fontWeight: 500 }}>
+            {participant.firstName} {participant.lastName}
+          </p>
+          {participant.lookingFor ? (
+            <div style={{ marginTop: -10 }}>
+              <span className="participant-card-marketplaceprofile--title">
+                Looking For:
+              </span>
+              {participant.lookingFor.map((profesion, i) => (
+                <SpecialtyItem key={i} title={profesion} />
+              ))}
+            </div>
+          ) : (
+            <p style={{ marginTop: -10 }}>{participant.titleProfessions}</p>
           )}
+          <p style={{ marginTop: -10 }}>{participant.company}</p>
 
+          {participant.location && marketplaceProfile && (
+            <div style={{ marginBottom: "50px" }}>
+              <span className="participant-card-marketplaceprofile--title">
+                Available locations:
+              </span>
+
+              {participant.location.map((location, i) => (
+                <SpecialtyItem key={i} title={location} />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      {participant && (
+        <div className="participant-card-marketplaceprofile">
           <Tooltip title="Linkedin">
             <Button
               shape="circle"
@@ -95,7 +69,6 @@ const CouncilParticipantCard = ({
                 window.open(participant.personalLinks.linkedin, "_blank")
               }
               className="participant-card-marketplaceprofile-icon"
-              disabled={userProfile.memberShip !== "premium"}
             />
           </Tooltip>
 
@@ -108,7 +81,6 @@ const CouncilParticipantCard = ({
                 window.open(`mailto:${participant.email}`, "_blank")
               }
               className="participant-card-marketplaceprofile-icon"
-              disabled={userProfile.memberShip !== "premium"}
             />
           </Tooltip>
         </div>
@@ -156,14 +128,7 @@ const ParticipantCard = ({
           />
         </>
       ) : (
-        <a
-          href={participant.personalLinks.linkedin}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="link-container"
-        >
-          <CouncilParticipantCard participant={participant} />
-        </a>
+        <CouncilParticipantCard participant={participant} />
       )}
     </Card>
   );
