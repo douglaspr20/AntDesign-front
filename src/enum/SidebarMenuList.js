f4926dd01e7088b491b7eb4ee284d74b192010fa
Juan Camilo Martinez Morales <xogost@gmail.com>
2022-01-11 19:33:25 -0500

    The talent marketplace menu was enabled

--- src/enum/SidebarMenuList.js
+++ src/enum/SidebarMenuList.js
@@ -16,7 +16,7 @@ import IconConferenceVideo from "images/icon-sidebar-video.svg";
 import IconTvOutline from "images/icon-tv-outline.svg";
 import IconGlobal from "images/icon-global.svg";
 import IconFlask from "images/icon-flask-outline.svg"
-// import IconBriefcase from "images/icon-briefcase-outline.svg"
+import IconBriefcase from "images/icon-briefcase-outline.svg"
 // import IconGlobal from "images/icon-global.svg";
 
 export default {
@@ -26,13 +26,11 @@ export default {
       icon: IconHome,
       url: INTERNAL_LINKS.HOME,
     },
-    /*
     {
       label: "Talent Marketplace",
       icon: IconBriefcase,
       url: INTERNAL_LINKS.TALENT_MARKETPLACE,
     },
-    */
     {
       label: "Global Conference",
       icon: IconGlobal,
