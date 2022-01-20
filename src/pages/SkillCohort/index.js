c65bd374d2b31872e05784e5758669cbcde21bc1
lourencelinao13 <lourencelinao13@gmail.com>
2022-01-21 01:53:17 +0800

    ProjectX

--- src/pages/SkillCohort/index.js
+++ src/pages/SkillCohort/index.js
@@ -5,6 +5,7 @@ import qs from "query-string";
 import { Tabs } from "components";
 import { useLocation } from "react-router-dom";
 import { INTERNAL_LINKS } from "enum";
+import { Space } from "antd";
 
 import { homeSelector } from "redux/selectors/homeSelector";
 import { skillCohortSelector } from "redux/selectors/skillCohortSelector";
@@ -102,12 +103,91 @@ const SkillCohort = ({
     return <SkillCohortCard key={skillCohort.id} skillCohort={skillCohort} />;
   });
 
+  const displayGeneralInformation = () => {
+    return (
+      <div style={{ marginTop: "1rem" }}>
+        <Space direction="vertical" size="large">
+          <Space direction="vertical">
+            <div>Hi {`${userProfile.firstName} ${userProfile.lastName}`},</div>
+            <div>Welcome to Hacking HR’s ProjectX!</div>
+            <div>
+              Thank you for checking out this page to find out more about one of
+              our flagship learning programs.{" "}
+            </div>
+          </Space>
+          <div>
+            <Space direction="vertical">
+              <h3>Project X</h3>
+              <div>
+                ProjectX is a cohort-based, skill-focused learning program
+                designed to help you learn or improve your knowledge in the
+                specific cohort-skills. We do this through daily resources that
+                will help you learn and encourage you to reflect about the
+                lessons learned and how to apply them to your daily professional
+                work.
+              </div>
+            </Space>
+          </div>
+          <div>
+            <Space direction="vertical">
+              <h3>How it works</h3>
+              <div>
+                This program is intense, but light touch: we will provide a
+                daily resource that you should be able to read, listen or watch
+                in less than 20 minutes.
+              </div>
+              <div>
+                After reading, listening or watching the resource, you will
+                provide a personal reflection about what you learned and how you
+                plan to apply the lessons learned.{" "}
+              </div>
+              <div>
+                This cohort program lasts 66 consecutive days (yes, consecutive
+                means every day, including weekends and any holidays that fall
+                in between). Each day, for 66 consecutive days, you will receive
+                via email and the cohort dashboard a daily resource. You need to
+                provide your daily reflection also in the dashboard.{" "}
+              </div>
+              <div>
+                This approach to learning is fast, but also allows for lots of
+                flexibility. Reading/watching/listening to the daily resource
+                and providing your reflection won’t take more than 20-25 minutes
+                per day… Hey, “excellence is a habit”… and we aim to make
+                light-touch learning a habit with discipline and commitment.{" "}
+              </div>
+              <div>
+                The program relies on daily consistency and discipline. Instead
+                of bugging you with heavy daily resources, “self-pace” learning
+                program from which you disengage too soon or long programs that
+                lasts for hours and months, we only ask you for 20-25 minutes or
+                less on a daily basis for 66 days. That’s it!{" "}
+              </div>
+            </Space>
+          </div>
+          <div>
+            <Space direction="vertical">
+              <h3>The Conditions</h3>
+              <div>
+                Please note: you MUST complete all the requirements of the
+                program and the completion will be assessed weekly. If in any
+                given week you fail to complete two or more reflections,
+                unfortunately you will be removed from the program and won’t be
+                able to access the dashboard anymore and won’t receive the
+                learning resources either.
+              </div>
+              <div>We hope you join us! </div>
+              <div>Thank you! </div>
+            </Space>
+          </div>
+        </Space>
+      </div>
+    );
+  };
+
   const TabData = [
     {
       title: "General Information",
-      content: () => {
-        return <div className="skill-cohort-list">General Information</div>;
-      },
+      content: displayGeneralInformation,
     },
     {
       title: "All Cohorts",
