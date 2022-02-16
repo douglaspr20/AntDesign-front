33da89292f7e852a6fb096021d5f03447d4fcff1
Douglas Perez <douglas.eduardo2000@gmail.com>
2022-02-16 14:01:54 -0400

    modified dates

--- src/pages/GlobalConference/index.js
+++ src/pages/GlobalConference/index.js
@@ -74,7 +74,7 @@ const GlobalConference = ({
   location,
 }) => {
   const [currentTab, setCurrentTab] = useState("0");
-  const [firstTabDate] = useState(moment("2022-02-10", "YYYY-MM-DD"));
+  const [firstTabDate] = useState(moment("2022-02-16", "YYYY-MM-DD"));
   const [tabData, setTabData] = useState([]);
   const [filters, setFilters] = useState({});
   const [meta, setMeta] = useState("");
