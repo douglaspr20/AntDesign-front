b7b7534671daadb750ebbf9812822321d866ce1c
Douglas Perez <douglas.eduardo2000@gmail.com>
2022-02-28 13:01:27 -0400

    component

--- src/components/index.js
+++ src/components/index.js
@@ -56,4 +56,5 @@ export { default as SpeakerCard } from "./SpeakerCard";
 export { default as ParticipantCard } from "./ParticipantCard";
 export { default as CategoriesSelect } from "./CategoriesSelect";
 export { default as UploadResumeModal } from "./UploadResumeModal";
+export { default as Chat } from "./Chat";
 export { default as BusinessDocumentsCard } from "./BusinessDocumentsCards";
