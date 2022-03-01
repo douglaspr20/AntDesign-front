b93e8a5572f040674e21d138f2e32e09455859a5
Betsy Moreno <morenoelba2002@gmail.com>
2022-03-01 11:40:56 -0400

    community-education: fix general responsive

--- src/pages/Council/index.js
+++ src/pages/Council/index.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
+import { Col, Row } from "antd";
 
 import { homeSelector } from "redux/selectors/homeSelector";
 import { councilSelector } from "redux/selectors/councilSelector";
+import Emitter from "services/emitter";
+import { EVENT_TYPES, INTERNAL_LINKS } from "enum";
+
+import FilterDrawer from "pages/Library/FilterDrawer";
 import { LibraryFilterPanel, Tabs } from "components";
 import NoItemsMessageCard from "components/NoItemsMessageCard";
-import { INTERNAL_LINKS } from "enum";
+import CouncilMembers from "./CouncilMembers";
+import CouncilList from "./CouncilList.js";
 
 import IconBack from "images/icon-back.svg";
 
-import CouncilMembers from "./CouncilMembers";
-import CouncilList from "./CouncilList.js";
 import "./style.scss";
 
 const CouncilPage = ({ userProfile, councilResources }) => {
@@ -40,7 +44,7 @@ const CouncilPage = ({ userProfile, councilResources }) => {
     //   title: "Conversations",
     //   content: () => (
     //     <NoItemsMessageCard
-    //       message={`There are no 
+    //       message={`There are no
     //     conversations
     //   for you at the moment`}
     //     />
@@ -52,29 +56,52 @@ const CouncilPage = ({ userProfile, councilResources }) => {
     },
   ];
 
+  const showFilterPanel = () => {
+    Emitter.emit(EVENT_TYPES.OPEN_FILTER_PANEL);
+  };
+
   return (
     <>
       {userProfile.councilMember || userProfile.role === "admin" ? (
         <div className="council-page">
-          <LibraryFilterPanel onChange={onFilterChange} />
-          <div className="council-page__container">
-            <div className="council-page__results">
-              <div className="council-page__row">
-                <div className="council-page__info-column"></div>
-                <div className="council-page__content">
-                  <Link to={INTERNAL_LINKS.HOME}>
-                    <div className="council-page__content-top">
-                      <div className="council-page__content-top-back">
-                        <img src={IconBack} alt="icon-back" />
-                      </div>
-                      <h4>Back</h4>
+          <div className="council-filter-panel">
+            <LibraryFilterPanel onChange={onFilterChange} />
+          </div>
+          <FilterDrawer onChange={onFilterChange} />
+          <div className="search-results-container">
+            <Row>
+              <Col span={24}>
+                <div className="search-results-container-mobile-header">
+                  <h3 className="filters-btn" onClick={showFilterPanel}>
+                    Filters
+                  </h3>
+                  {/* <h3>{`${numberWithCommas(countOfResults)} result${
+                countOfResults > 1 ? "s" : ""
+              }`}</h3> */}
+                </div>
+              </Col>
+            </Row>
+            <div className="council-page__container">
+              <div className="council-page__results">
+                <div className="council-page__row">
+                  <div className="council-page__info-column"></div>
+                  <div className="council-page__content">
+                    <div className="council-filter-panel">
+                      <Link to={INTERNAL_LINKS.HOME}>
+                        <div className="council-page__content-top">
+                          <div className="council-page__content-top-back">
+                            <img src={IconBack} alt="icon-back" />
+                          </div>
+                          <h4>Back</h4>
+                        </div>
+                      </Link>
                     </div>
-                  </Link>
-                  <Tabs
-                    data={TabData}
-                    current={currentTab}
-                    onChange={setCurrentTab}
-                  />
+                    <Tabs
+                      data={TabData}
+                      current={currentTab}
+                      onChange={setCurrentTab}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
