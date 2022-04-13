b354fda71a37caf8461f9dc23b1eea83b0239520
Lourence Linao <lourencelinao13@gmail.com>
2022-04-13 22:56:25 +0800

    [FEATURE][Lourence] - Advertisement

--- src/pages/SponsorDashboard/Advertiser/index.js
+++ src/pages/SponsorDashboard/Advertiser/index.js
@@ -60,6 +60,11 @@ const Advertiser = ({
       },
     },
     {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
       title: "Status",
       dataIndex: "status",
       key: "status",
